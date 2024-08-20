6d6948a526c2709797b794c5a0f5f0a8d0c0ba6b
TieZhuGieGie <dcx1378832571@163.com>
2024-08-20 17:32:20 +0800

    ALL

---
+++ Questionnaire/src/components/NavigationBarInPersonal.vue
@@ -0,0 +1,131 @@
+<!-- 导航栏 -->
+<!-- 请作为组件引入到首页，即 HomeView.vue -->
+<script setup>
+    import { ref, watchEffect, computed } from 'vue';
+    import { useRouter } from 'vue-router';
+    import { ArrowDown } from '@element-plus/icons-vue'
+    import store from '../store';
+
+    const router = useRouter();
+
+    const gotoLogin = () => {
+        router.push('/login');
+    };
+
+    const gotoHome = () => {
+        // store.state.nowuser.username = "";
+        const cookieNames = $cookies.keys();
+        // 遍历删除所有的 cookie
+        cookieNames.forEach(cookieName => {
+            $cookies.remove(cookieName);
+        });
+        router.push('/');
+    }
+
+    const gotoPersonal = () => {
+        router.push('/personal');
+    }
+
+    const gotoUserManage = () => {
+        router.push('/userManage');
+    }
+
+    const goBack = () => {
+        router.go(-1); // 返回上一页
+    };
+
+    // const showBackButton = () => {
+    //     // 根据当前路由判断是否显示返回按钮
+    //     return router.currentRoute.value.path !== '/userManage';
+    // };
+
+    function logoIn(elementId) {
+        document.getElementById(elementId).classList.add("zoom-btn");
+    }
+
+    function logoOut(elementId) {
+        document.getElementById(elementId).classList.remove("zoom-btn");
+    }
+
+    const currentUrl = ref(window.location.href);
+    const isHomePage = ref(false);
+    watchEffect(() => { // 在组件挂载时检查当前页面 URL，判断是否为根目录页面
+        isHomePage.value = currentUrl.value === window.location.origin;
+    });
+
+    window.addEventListener('popstate', () => {
+        currentUrl.value = window.location.href;
+    });
+
+</script>
+
+<template>
+    <el-header class = "navigationBar">
+        <div @click = "gotoUserManage" @mouseover="logoIn('logo')" @mouseout="logoOut('logo')">
+            <img id="logo" src="../assets/logo.png" alt="Element logo" class = "logo"/>
+        </div>
+        <div class="title">
+            <span>纸翼</span>传问
+        </div>
+        <div class="flex-grow"></div>
+    </el-header>
+</template>
+
+<style lang = "scss" scoped>
+    .navigationBar {
+        display: flex;
+        align-items: center;
+        // justify-content: center;
+        height: 90px;
+        // position: fixed; /* 将导航栏固定在页面上 */
+        top: 0; /* 从页面顶部开始定位 */
+        left: 0; /* 从页面左侧开始定位 */
+        width: 100%; /* 宽度占据整个页面 */
+        height: 60px; /* 设置导航栏高度 */
+        z-index: 1000; /* 设置层叠顺序，确保导航栏位于其他内容之上 */
+        margin: 5px 0 0 7px;
+
+        .logo {
+            width: 60px;
+            cursor: pointer;
+            margin-top: 5px;
+            // border: red solid 1px;
+        }
+
+        .title {
+            font-size: 26px;
+            margin-left: 9px;
+            color: white;
+            span {
+                color: rgb(243,156,181);
+                font-weight: bold;
+            }
+        }
+
+        .zoom-btn {
+            transform: scale(1.2); /* 缩放图片到 1.2 倍大小 */
+            transition: transform 0.3s ease-in-out; /* 添加过渡效果 */
+        }
+
+        .btn {
+            // width: 5%;
+            height: 40px;
+            background: rgb(97, 187, 252);
+            color:white;
+            border: none;
+            outline: none;
+            border-radius: 5px;
+            // letter-spacing: 10px;
+            font-size: 18px;
+            cursor: pointer;
+            margin: 5px 10px 0 0px;
+        }
+
+
+    }
+
+    .flex-grow {
+        flex-grow: 1;
+    }
+</style>
+  
---
+++ Questionnaire/src/main.js
@@ -0,0 +1,39 @@
+import './assets/main.scss'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 引入 ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+// 引入 vant
+import { RadioGroup, Radio } from 'vant'; 
+import { Checkbox, CheckboxGroup } from 'vant';
+import { Divider } from 'vant';
+
+import VueCookies from 'vue-cookies'
+
+//引入Echarts
+import * as echarts from 'echarts';
+//引入打印
+import Print from 'vue3-print-nb';
+
+const app = createApp(App)
+
+app.config.globalProperties.$echarts = echarts // 全局挂载echarts
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(store).use(router).use(ElementPlus,{locale: zhCn}).use(RadioGroup).use(Radio).use(Checkbox).use(CheckboxGroup).use(Divider).use(echarts).use(Print).mount('#app')
+
+
+app.config.globalProperties.$cookies = VueCookies
+VueCookies.config("1d");
