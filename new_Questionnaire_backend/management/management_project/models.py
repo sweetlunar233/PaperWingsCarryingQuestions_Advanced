ca57d57912944f1d5b0bdd23d2838d2b3bd971c5
LYWen <1658441344@qq.com>
2024-08-21 12:10:55 +0800

    拆分数据库表

--- new_Questionnaire_backend/management/management_project/models.py
+++ new_Questionnaire_backend/management/management_project/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Survey(models.Model):
+    SurveyID = models.AutoField(primary_key=True)
+    OwnerID = models.IntegerField(unique=True)
+    Title = models.CharField(max_length=200)
+    Description = models.TextField(max_length=500, blank=True)
+    Is_released = models.BooleanField(blank=True)
+    Is_open = models.BooleanField(default=True)
+    Is_deleted=models.BooleanField(default=False)
+    #新加的（把日期变为字符串）
+    PublishDate = models.DateTimeField(null=True)
+    #0 是普通问卷，1是投票问卷，2是报名问卷，3是考试问卷
+    Category = models.IntegerField(default=0)   
+    TotalScore = models.IntegerField(null=True, blank=True)
+    TimeLimit = models.IntegerField(null=True, blank=True)
+    IsOrder = models.BooleanField(default=True)
+    #QuotaLimit = models.IntegerField(null=True, default=False)
+
+class Submission(models.Model):
+    SubmissionID = models.AutoField(primary_key=True)
+    SurveyID = models.IntegerField(unique=True)
+    RespondentID = models.IntegerField(unique=True)
+    SubmissionTime = models.DateTimeField(auto_now_add=True)
+    Status = models.CharField(max_length=20, choices=[('Unsubmitted', 'Unsubmitted'), ('Submitted', 'Submitted'), ('Graded', 'Graded'),('Deleted','Deleted')])
+    Score = models.IntegerField(null=True, blank=True)
+    #Duration = models.IntegerField(null=True, blank=True)
+    Interval=models.IntegerField(null=True, blank=True)
--- new_Questionnaire_backend/user/user_project/models.py
+++ new_Questionnaire_backend/user/user_project/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import json
 
 # Create your models here.
+class User(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=50)
+    password = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, max_length=100)
+    CreateDate = models.DateTimeField(auto_now_add=True)
+    isActive=models.BooleanField(default=False)
+    zhibi=models.IntegerField(default=0)
+    own_photos=models.TextField(default=json.dumps([0] * 18))
+
+    def set_array_element(self, index, value):
+        # 确保索引在有效范围内  
+        if 0 <= index < 18:  
+            photos_data = json.loads(self.own_photos)  
+            photos_data[index] = value  
+            self.own_photos = json.dumps(photos_data)  
+            self.save()  
+    
+    def get_array_element(self, index):  
+        # 确保索引在有效范围内  
+        if 0 <= index < 18:  
+            photos_data = json.loads(self.own_photos)  
+            return photos_data[index]  
+        return -1
+    
+    #获取当前正在使用的头像编号(默认为0，1是已购买，2是正在使用)
+    def get_used_element(self):
+        photos_data = json.loads(self.own_photos)
+        for i in range(0,18):
+            if(photos_data[i]==2): return i
+        return -1
